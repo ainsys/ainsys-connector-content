3f73eda98a4e719ff0bb5a416473121758794be7
Артем <artikus.sol@gmail.com>
2022-10-04 03:37:55 +0400

    Init

---
+++ includes/class-plugin.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Ainsys\Connector\Content;
+
+use Ainsys\Connector\Content\Webhooks\Handle_Replace_Content;
+use Ainsys\Connector\Master\Core;
+use Ainsys\Connector\Master\Hooked;
+use Ainsys\Connector\Master\Logger;
+use Ainsys\Connector\Master\Plugin_Common;
+use Ainsys\Connector\Master\Settings\Settings;
+use Ainsys\Connector\Master\Settings\Admin_UI;
+
+class Plugin implements Hooked {
+
+	use Plugin_Common;
+
+	/**
+	 * @var Core
+	 */
+	private Core $core;
+
+	/**
+	 * @var Logger
+	 */
+	private Logger $logger;
+
+	/**
+	 * @var Settings
+	 */
+	private Settings $settings;
+
+	/**
+	 * @var Admin_UI
+	 */
+	private Admin_UI $admin_ui;
+
+
+	public function __construct( Core $core, Logger $logger, Settings $settings, Admin_UI $admin_ui ) {
+
+		$this->core     = $core;
+		$this->logger   = $logger;
+		$this->settings = $settings;
+		$this->admin_ui = $admin_ui;
+
+		$this->init_plugin_metadata();
+
+		$this->components['replace_content_webhook'] = new Handle_Replace_Content();
+	}
+
+
+	/**
+	 * Links all logic to WP hooks.
+	 *
+	 * @return void
+	 */
+	public function init_hooks() {
+
+		add_filter( 'ainsys_status_list', [ $this, 'add_status_of_component' ], 10, 1 );
+
+		foreach ( $this->components as $component ) {
+			if ( $component instanceof Hooked ) {
+				$component->init_hooks();
+			}
+		}
+	}
+
+
+	/**
+	 * Generates a component status to show on the General tab of the master plugin settings.
+	 *
+	 * @return array
+	 */
+	public function add_status_of_component( $status_items = [] ) {
+
+		$status_items['replace_content'] = [
+			'title'  => __( 'Replace Content', AINSYS_CONNECTOR_CONTENT_TEXTDOMAIN ),
+			'active' => true,
+		];
+
+		return $status_items;
+	}
+
+}
---
+++ includes/webhooks/class-handle-replace-content.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Ainsys\Connector\Content\Webhooks;
+
+use Ainsys\Connector\Master\Hooked;
+use Ainsys\Connector\Master\Webhook_Handler;
+
+class Handle_Replace_Content implements Hooked, Webhook_Handler {
+
+	public function __construct() {
+	}
+
+
+	/**
+	 * Initializes WordPress hooks for component.
+	 *
+	 * @return void
+	 */
+	public function init_hooks(): void {
+
+		add_filter( 'ainsys_webhook_action_handlers', [ $this, 'register_webhook_handler' ], 10, 1 );
+
+		add_filter( 'the_content', [ $this, 'replace_content' ], 100 );
+	}
+
+
+	public function register_webhook_handler( $handlers = [] ) {
+
+		$handlers['AINSYS_CMS_main'] = [ $this, 'handler' ];
+
+		return $handlers;
+	}
+
+
+	/**
+	 * @param  string $action
+	 * @param  array  $data
+	 * @param  int    $object_id
+	 *
+	 * @return string
+	 */
+	public function handler( string $action, array $data, int $object_id ): string {
+
+		$data    = (array) $data;
+		$message = 'Action not registered';
+
+		switch ( $action ) {
+			case 'CREATE':
+			case 'UPDATE':
+				if ( $data['pageId'] && $this->is_local( $data['pageLang'] ) ) {
+
+					$this->update_entity_data( $data );
+
+					$message = 'The action has been completed successfully. Content imported';
+				} else {
+					$message = 'Page id is missing';
+				}
+
+				break;
+			case 'delete':
+
+		}
+
+		return $message;
+	}
+
+
+	/**
+	 * @param  array $data
+	 *
+	 * @return void
+	 */
+	protected function update_entity_data( array $data ): void {
+
+		$sites = get_sites( [
+			'fields'        => 'ids',
+			'no_found_rows' => false,
+		] );
+
+		foreach ( $sites as $site_id ) {
+			switch_to_blog( $site_id );
+
+			$page = get_post( (int) $data['pageId'] );
+
+			if ( $page && ( 'post' === $page->post_type || 'page' === $page->post_type ) ) {
+				update_post_meta( $page->ID, '_ainsys_entity_data', $data );
+			}
+
+			restore_current_blog();
+
+		}
+	}
+
+
+	protected function is_local( $local ): bool {
+
+		return $this->data_local( $local ) === $this->current_local();
+	}
+
+
+	/**
+	 * @param  string $local
+	 *
+	 * @return string
+	 */
+	protected function data_local( string $local ): string {
+
+		$local = str_replace( '/', '', $local );
+
+		return mb_strtolower( str_replace( '-', '_', $local ), 'UTF-8' );
+	}
+
+
+	/**
+	 * @return string
+	 */
+	protected function current_local(): string {
+
+		return mb_strtolower( str_replace( '-', '_', get_locale() ), 'UTF-8' );
+	}
+
+
+	/**
+	 * Replaces codes like {xxx} with jso values in the text
+	 *
+	 * @param  string $text - text to search/replace.
+	 *
+	 * @package ainsys
+	 */
+	public function replace_content( string $text ): string {
+
+
+		$data = get_post_meta( get_the_ID(), '_ainsys_entity_data', true );
+
+		if ( $data ) {
+
+			$text = $this->get_data_ainsys( $data, $text );
+
+		} else {
+
+			$text = $this->get_data_file( $text );
+		}
+
+		return $text;
+	}
+
+
+	/**
+	 * @param         $data
+	 * @param  string $text
+	 *
+	 * @return string
+	 */
+	protected function get_data_ainsys( $data, string $text ): string {
+
+		$keys   = [];
+		$values = [];
+
+		foreach ( $data as $key => $value ) {
+
+			if ( is_string( $value ) ) {
+				$keys[]   = $key;
+				$values[] = $value;
+			}
+
+		}
+
+		return str_replace( $keys, $values, $text );
+	}
+
+
+	/**
+	 * @param  string $text
+	 *
+	 * @return string
+	 */
+	protected function get_data_file( string $text ): string {
+
+		$file = get_field( 'json_file' ) ? trim( get_field( 'json_file' ) ) : '';
+
+		if ( $file ) {
+			$json = json_decode( file_get_contents( $file ), true );
+
+			$keys   = [];
+			$values = [];
+
+			foreach ( $json as $key => $value ) {
+				$keys[]   = $key;
+				$values[] = trim( str_replace( '\n', '', $value ) );
+			}
+
+			return str_replace( $keys, $values, $text );
+		}
+
+		return $text;
+	}
+
+}
