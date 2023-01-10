8cf82ab38b169a6f8fdc83888a89b783a17839ad
Артем <artikus.sol@gmail.com>
2023-01-10 17:47:12 +0400

    VER - version && readme

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ namespace Ainsys\Connector\Content;
  * Plugin Name:       AINSYS Connector Headless CMS
  * Plugin URI: https://app.ainsys.com/
  * Description: Plugin for replacing content on a WordPress site.
- * Version:           1.0.6
+ * Version:           1.1.0
  * Author:            AINSYS
  * Author URI:        https://app.ainsys.com/
  * License: GPLv2 or later
